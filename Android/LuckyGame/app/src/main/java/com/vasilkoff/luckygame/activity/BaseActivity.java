74f35f0590afc93e1effae7e262cbb14e5f5283b
Volodymyr Kusenko <volodymyr.kusenko@gmail.com>
2017-02-17 15:48:00 +0200

    Added base and home activities

---
+++ Android/LuckyGame/app/src/main/java/com/vasilkoff/luckygame/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.vasilkoff.luckygame.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kusenko on 17.02.2017.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
--- Android/LuckyGame/app/src/main/java/com/vasilkoff/luckygame/activity/LoginActivity.java
+++ Android/LuckyGame/app/src/main/java/com/vasilkoff/luckygame/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.vasilkoff.luckygame;
+package com.vasilkoff.luckygame.activity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.vasilkoff.luckygame.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
