09fd48236cc149326316dac634fb615a10209e3a
Volodymyr Kusenko <volodymyr.kusenko@gmail.com>
2017-04-12 14:15:25 +0300

    Added header cover

--- Android/LuckyGame/app/src/main/java/com/vasilkoff/luckygame/activity/BaseActivity.java
+++ Android/LuckyGame/app/src/main/java/com/vasilkoff/luckygame/activity/BaseActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
@@ -73,6 +74,10 @@ public abstract class BaseActivity extends AppCompatActivity implements GoogleAp
                     .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                     .build();
         }
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
     boolean checkLogin() {
