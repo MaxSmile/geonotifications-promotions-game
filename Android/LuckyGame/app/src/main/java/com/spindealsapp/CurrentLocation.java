786802c38fe056f40831a9f09f1d792c47995760
Volodymyr Kusenko <volodymyr.kusenko@gmail.com>
2017-10-31 17:47:08 +0200

    Calculating better location

--- Android/LuckyGame/app/src/main/java/com/spindealsapp/CurrentLocation.java
+++ Android/LuckyGame/app/src/main/java/com/spindealsapp/CurrentLocation.java
@@ -1,12 +1,14 @@
 package com.spindealsapp;
 
+import android.location.Location;
+
 /**
  * Created by Kvm on 16.06.2017.
  */
 
 public class CurrentLocation {
+    public static Location location;
     public static double lat;
     public static double lon;
-    public static String provider;
     public static boolean check;
 }
--- Android/LuckyGame/app/src/main/java/com/spindealsapp/service/LocationService.java
+++ Android/LuckyGame/app/src/main/java/com/spindealsapp/service/LocationService.java
@@ -48,6 +48,7 @@ public class LocationService extends Service {
     private int requestCode = 100;
     private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATE = 10; // in Meters
     private static final long MINIMUM_TIME_BETWEEN_UPDATE = 5000; // in Milliseconds
+    private static final int TIME_DIFFERENCE_THRESHOLD = 60000; // in Milliseconds
 
     private static final long POINT_RADIUS = 1000; // in Meters
     private static final long PROX_ALERT_EXPIRATION = -1;
@@ -185,25 +186,9 @@ public class LocationService extends Service {
 
     public class MyLocationListener implements LocationListener {
         public void onLocationChanged(Location location) {
-            CurrentLocation.lat = location.getLatitude();
-            CurrentLocation.lon = location.getLongitude();
-            CurrentLocation.provider = location.getProvider();
-            if (!CurrentLocation.check) {
-                CurrentLocation.check = true;
-                ArrayList<Place> places = new ArrayList<Place>(DBHelper.getInstance(App.getInstance()).getPlaces().values());
-                for (int i = 0; i < places.size(); i++) {
-                    Place place = places.get(i);
-                    if (place.getGeoLat() != 0 && place.getGeoLon() != 0) {
-                        place.setDistanceString(LocationDistance.getDistance(CurrentLocation.lat, CurrentLocation.lon,
-                                place.getGeoLat(), place.getGeoLon()));
-                        place.setDistance(LocationDistance.calculateDistance(CurrentLocation.lat, CurrentLocation.lon,
-                                place.getGeoLat(), place.getGeoLon()));
-                    }
-                }
-
-                if (DBHelper.getInstance(App.getInstance()).updatePlaces(places)) {
-                    EventBus.getDefault().post(new Events.UpdateLocation());
-                }
+            if (isBetterLocation(location, CurrentLocation.location)) {
+                CurrentLocation.location = location;
+                changeLocation();
             }
         }
         public void onStatusChanged(String s, int i, Bundle b) {
@@ -212,6 +197,82 @@ public class LocationService extends Service {
         }
         public void onProviderEnabled(String s) {
         }
+    }
+
+    private void changeLocation() {
+        CurrentLocation.lat = CurrentLocation.location.getLatitude();
+        CurrentLocation.lon = CurrentLocation.location.getLongitude();
+        if (!CurrentLocation.check) {
+            CurrentLocation.check = true;
+            ArrayList<Place> places = new ArrayList<Place>(DBHelper.getInstance(App.getInstance()).getPlaces().values());
+            for (int i = 0; i < places.size(); i++) {
+                Place place = places.get(i);
+                if (place.getGeoLat() != 0 && place.getGeoLon() != 0) {
+                    place.setDistanceString(LocationDistance.getDistance(CurrentLocation.lat, CurrentLocation.lon,
+                            place.getGeoLat(), place.getGeoLon()));
+                    place.setDistance(LocationDistance.calculateDistance(CurrentLocation.lat, CurrentLocation.lon,
+                            place.getGeoLat(), place.getGeoLon()));
+                }
+            }
+
+            if (DBHelper.getInstance(App.getInstance()).updatePlaces(places)) {
+                EventBus.getDefault().post(new Events.UpdateLocation());
+            }
+        }
+    }
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    private boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TIME_DIFFERENCE_THRESHOLD;
+        boolean isSignificantlyOlder = timeDelta < -TIME_DIFFERENCE_THRESHOLD;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
     }
 
 }
